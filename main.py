74ff3c80bcda3123f862153fe388b1c4079bfae7
Kham Mangar <49294006+kmangar@users.noreply.github.com>
2022-04-19 13:18:07 -0400

    bs4 web scraping

--- main.py
+++ main.py
@@ -17,29 +17,34 @@ form_link = "https://forms.gle/82EErXMpxZj81u3Z6"
 zillow_link = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D"
 
 response = requests.get(zillow_link, headers=header)
-zillow_sf_rent = response.text
+data = response.text
 
-soup = BeautifulSoup(zillow_sf_rent, "html.parser")
+soup = BeautifulSoup(data, "html.parser")
 
 # print(soup.title)
 
 properties = soup.findAll(name="div", class_="list-card-info")
 
+# TODO:: create a list with all the links and if it's missing parts in the link fill it in
+all_link_elements = soup.select(".list-card-top a")
+all_links = []
+for link in all_link_elements:
+    href = link["href"]
+    # print(href)
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
 
-# print(soup)
-# pprint(zillow_sf_rent)
-list_address = [addi.getText() for addi in soup.findAll(name="address", class_="list-card-addr")]
-list_link = [link.getText("href") for link in soup.findAll(name="a", class_="list-card-link-top-margin")]
+# TODO: store all the address in a list. clean the address formatting
+list_address = [address.get_text().split(" | ")[-1] for address in soup.findAll(name="address", class_="list-card-addr")]
 
-list_price = [price.getText() for price in soup.findAll(name="div", class_="list-card-price")]
+# TODO: store all the rental price in a list and clean formatting 
+list_price = [price.getText().split("+")[0] for price in soup.findAll(name="div", class_="list-card-price")]
 
-print(list_address)
-print(list_link)
-print(list_price)
+# output for visual testing purpose
+print(f"{list_address}\nlen: {len(list_address)}")
+print(f"{all_links}\nlen: {len(all_links)}")
+print(f"{list_price}\nlen: {len(list_price)}")
 
-#
-# driver = webdriver.Chrome("")
-#
-# driver.get()
-#
-# driver.find_element(By)
+# TODO: USING Selenium fill in the google form 
